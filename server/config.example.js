6c54861136b58c03328c6d37a03f19d7cd671442
Colin Chauche <colin.chauche@gmail.com>
2021-03-27 21:28:01 -0400

    Add instructions for adding API Token

---
+++ server/config.example.js
@@ -0,0 +1,16 @@
+/**
+ * Make a copy of this file and rename it 'config.js'. To use this repo you
+ * need to have a github token, please follow these instructions to creat a
+ * new token:
+ * 1. Go to: https://github.com/settings/tokens
+ * 2. Click "Generate new token"
+ * 3. Given the Token a Description ("Debugger Repo", or whatever is most descriptive to you)
+ * 4. Under Select Scopes, select at least the following:
+ *    - user
+ *    - read:user
+ *    - user:email
+ *    - user:follow
+ * 5. Replace YOUR TOKEN HERE with your token
+ * */
+
+ module.exports = "YOUR TOKEN HERE";
