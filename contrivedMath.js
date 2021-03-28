9ef419f7b571c273c26bdd116da92e9cb732dbe8
Colin Chauche <colin.chauche@gmail.com>
2021-03-27 20:17:55 -0400

    Add bug to contrived Math

--- contrivedMath.js
+++ contrivedMath.js
@@ -10,7 +10,7 @@ const add = (num1, num2) => {
 const multiply = (num1, num2) => {
   let result = 0;
   while (num2 > 0) {
-    result = add(result, num1);
+    result += add(result, num1);
     num2 -= 1;
   }
   return result
@@ -23,7 +23,7 @@ const raiseToRecursive = (base, exponent) => {
   return multiply(base, raiseToRecursive(base, exponent - 1))
 }
 
-console.log('Add 1 + 1: ', add(1,1));
-console.log('Multiply 2x5: ', multiply(2,5));
-console.log('Raise 2 to the 3rd: ', raiseToRecursive(2,5));
+console.log('Add 2 + 3 should equal 5: ',add(2,3) === 5);
+console.log('Multiply 2x5 should equal 10: ',multiply(2,5) === 10);
+console.log('Raise 2 to the 4th should equal 16: ', raiseToRecursive(2,4) === 16);
 console.log('Adds Done: ', addsDone);
